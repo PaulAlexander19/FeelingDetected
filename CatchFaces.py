6573f71ee7f905d4196803d5ad89b12c56f7b97f
Paul Alexander <pluquec@unsa.edu.pe>
2021-11-11 00:50:51 -0500

    Convirtiendo la imagen a blanco y negro

--- CatchFaces.py
+++ CatchFaces.py
@@ -2,7 +2,8 @@ import cv2
 
 
 img = cv2.imread('./data/faces/amigos.jpg')     ## Load the image
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)   ## Convert to grayscale
 
-cv2.imshow('img', img)      ## Show the image
+cv2.imshow('img', gray)      ## Show the image
 cv2.waitKey(5000)           ## Wait for 5 seconds
 cv2.destroyAllWindows()     ## Close all windows

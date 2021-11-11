5a24b5d270b6fc607f7d7208bd150a36f89d49da
Paul Alexander <pluquec@unsa.edu.pe>
2021-11-11 01:22:00 -0500

    Identificando los rostros de las fotos

--- CatchFaces.py
+++ CatchFaces.py
@@ -1,9 +1,20 @@
 import cv2
 
-
+faceClassif = cv2.CascadeClassifier('/home/pool/.local/lib/python3.8/site-packages/cv2/data/haarcascade_frontalface_default.xml')
 img = cv2.imread('./data/faces/amigos.jpg')     ## Load the image
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)   ## Convert to grayscale
 
-cv2.imshow('img', gray)      ## Show the image
+
+faces = faceClassif.detectMultiScale(gray,
+                                    scaleFactor=1.1,    ## Scale factor
+                                    minNeighbors=5,     ## Minimum neighbors
+                                    minSize=(30, 30),   ## Minimum size
+                                    maxSize=(80, 80))   ## Maximum size
+
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)      ## Draw rectangle around the face
+
+cv2.imshow('img', img)      ## Show the image
 cv2.waitKey(5000)           ## Wait for 5 seconds
 cv2.destroyAllWindows()     ## Close all windows

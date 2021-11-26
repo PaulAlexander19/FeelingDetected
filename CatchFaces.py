23b836cbd1ffe6ec3c114a6a284e6d63c3f9ded9
Paul Alexander <pluquec@unsa.edu.pe>
2021-11-26 01:32:41 -0500

    Guarda capturas del rostro de la persona que aparece en el video

--- CatchFaces.py
+++ CatchFaces.py
@@ -1,25 +1,44 @@
 import cv2
+import os
 
+## Crear el directorio
+if not os.path.exists("faces_found"):
+    print("Carpeta creada")
+    os.makedirs("faces_found")
+    
 cap = cv2.VideoCapture(1)       ## 0 for default camera
 
 faceClassif = cv2.CascadeClassifier('./Models/haarcascade_frontalface_default.xml')     ## Load the classifier
 
 
+count = 0
 while True:     ### While loop to keep the camera on
     ret, frame = cap.read()     ## Read the frame    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)   ## Convert to grayscale
-
+    
+    auxFrame = frame.copy()
+    
 
     faces = faceClassif.detectMultiScale(gray, 1.1, 5)     ## Detect faces
 
-
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+    
     for (x, y, w, h) in faces:   ## For each face
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)      ## Draw rectangle around the face
-
+        rostro = auxFrame[y:y+h, x:x+w]
+        rostro = cv2.resize(rostro, (150, 150), interpolation=cv2.INTER_CUBIC)
+        
+        # if k == ord('s'):
+        cv2.imwrite("faces_found/rostro_{}.jpg".format(count), rostro)
+        cv2.imshow("rostro", rostro)
+        count += 1
+        
+        
     cv2.imshow('img', frame)      ## Show the image
     
-    if cv2.waitKey(1) & 0xFF == ord('q'):     ## Wait for 'q' to quit
-        break
+   
     
 cv2.waitKey(5000)           ## Wait for 5 seconds
 cv2.destroyAllWindows()     ## Close all windows

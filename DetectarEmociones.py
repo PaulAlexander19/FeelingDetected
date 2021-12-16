eff8109a21b7542618289c6920bba5ff867db92c
Paul Alexander <pluquec@unsa.edu.pe>
2021-12-16 17:49:04 -0500

    Corrigiendo el codigo de la clase DetectarEmociones para que pueda usar otros modelos

--- DetectarEmociones.py
+++ DetectarEmociones.py
@@ -5,7 +5,19 @@ import numpy as np
 # ----------- Métodos usados para el entrenamiento y lectura del modelo ----------
 method = 'LBPH'
 locationModel = "./modelEmotion"
-emotion_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+if(method == "EigenFaces"): 
+	emotion_recognizer = cv2.face.EigenFaceRecognizer_create() ## ceamos el modelo
+
+elif(method == "FisherFaces"): 
+	emotion_recognizer = cv2.face.FisherFaceRecognizer_create() ## ceamos el modelo
+
+elif(method == "LBPH"): 
+	emotion_recognizer = cv2.face.LBPHFaceRecognizer_create() ## ceamos el modelo
+else:
+	print("No encontrada la emción, trabajando con LBPH") 
+	emotion_recognizer = cv2.face.LBPHFaceRecognizer_create() ## ceamos el modelo
+
 
 emotion_recognizer.read(locationModel +'/modelo'+method+'.xml')
 # --------------------------------------------------------------------------------
@@ -29,7 +41,7 @@ while True:
 
 	# nFrame = cv2.hconcat([frame, np.zeros((480,300,3),dtype=np.uint8)])
 
-	faces = faceClassif.detectMultiScale(gray,1.3,4)
+	faces = faceClassif.detectMultiScale(gray,1.2,5)
 
 	for (x,y,w,h) in faces:
 		rostro = auxFrame[y:y+h,x:x+w]
@@ -38,20 +50,34 @@ while True:
 
 		cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
 
+		# EigenFaces
+		if (method == 'EigenFaces'):
+			if result[1] < 5700:
+				cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+				cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+			else:
+				cv2.putText(frame,'No identificado',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+				cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
 
 	  
 		# LBPHFace
 		if method == 'LBPH':
-			if result[1] < 70:
+			if result[1] < 90:
 				cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
 				cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-				# image = emotionImage(imagePaths[result[0]])
-				# nFrame = cv2.hconcat([frame,image])
 			else:
 				cv2.putText(frame,'No identificado',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
 				cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-				# nFrame = cv2.hconcat([frame,np.zeros((480,300,3),dtype=np.uint8)])
 
+		# FisherFace
+		if method == 'FisherFaces':
+			if result[1] < 500:
+				cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+				cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+			else:
+				cv2.putText(frame,'No identificado',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+				cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+        
 	cv2.imshow('Frande',frame)
 	k = cv2.waitKey(1)
 	if k == 27:

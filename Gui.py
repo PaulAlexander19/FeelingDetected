215282529068b89f6342ffad2b40c8f0d2d56306
Paul Alexander <pluquec@unsa.edu.pe>
2021-12-17 08:52:32 -0500

    Comentando GUI

--- Gui.py
+++ Gui.py
@@ -6,12 +6,12 @@ from DetectarEmociones import detectionEmotion
 import cv2
 import imutils
 
-# faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-# emotion_recognozer = getEmotion_recognozer()
-
+## Reconoce la entrada del video
 def video_de_entrada():
     global cap
+    ## Seleccion un video local
     if selected.get() == 1:
+        ## IDentificamos el tipo de archivo
         path_video = filedialog.askopenfilename(filetypes = [
             ("all video format", ".mp4"),
             ("all video format", ".avi")])
@@ -24,11 +24,13 @@ def video_de_entrada():
             lblInfoVideoPath.configure(text=pathInputVideo)
             cap = cv2.VideoCapture(path_video)
             visualizar()
+    ## Activa la camara web
     if selected.get() == 2:
         btnEnd.configure(state="active")
         rad1.configure(state="disabled")
         rad2.configure(state="disabled")
         lblInfoVideoPath.configure(text="")
+        ## Abrimos la camara
         try:
             cap = cv2.VideoCapture(0)
         except:
@@ -68,23 +70,23 @@ def finalizar_limpiar():
     
     
 cap = None
-root = Tk()
-root.title("Deteccion de Rostros")
-lblInfo1 = Label(root, text="Detección de Emociones", font="bold")
-lblInfo1.grid(column=0, row=0, columnspan=2)
+root = Tk() ## Crear la ventana
+root.title("Deteccion de Rostros") ## Titulo de la ventana
+lblInfo1 = Label(root, text="Detección de Emociones", font="bold") ## Agregar una etiqueta
+lblInfo1.grid(column=0, row=0, columnspan=2) ## Ubicar la etiqueta
 
-selected = IntVar()
-rad1 = Radiobutton(root, text="Elegir video", width=20, value=1, variable=selected, command=video_de_entrada)
-rad2 = Radiobutton(root, text="Video en directo", width=20, value=2, variable=selected, command=video_de_entrada)
-rad1.grid(column=0, row=1)
-rad2.grid(column=1, row=1)
+selected = IntVar()  ## Crear una variable de tipo IntVar
+rad1 = Radiobutton(root, text="Elegir video", width=20, value=1, variable=selected, command=video_de_entrada) ## Crear un Radiobutton
+rad2 = Radiobutton(root, text="Video en directo", width=20, value=2, variable=selected, command=video_de_entrada) ## Crear un Radiobutton
+rad1.grid(column=0, row=1)  ## Ubicar el Radiobutton
+rad2.grid(column=1, row=1) ## Ubicar el Radiobutton
 
 
-lblInfoVideoPath = Label(root, text="", width=20)
-lblInfoVideoPath.grid(column=0, row=2)
+lblInfoVideoPath = Label(root, text="", width=20) ## Agregar una etiqueta para identificar la ruta del video
+lblInfoVideoPath.grid(column=0, row=2) ## Ubicar la etiqueta
 
-lblVideo = Label(root)
-lblVideo.grid(column=0, row=3, columnspan=2)
+lblVideo = Label(root) ## Agregar una etiqueta para visualizar el video
+lblVideo.grid(column=0, row=3, columnspan=2) ## Ubicar la etiqueta
 
 btnEnd = Button(root, text="Finalizar visualización y limpiar", state="disabled", command=finalizar_limpiar)
 btnEnd.grid(column=0, row=4, columnspan=2, pady=10)
